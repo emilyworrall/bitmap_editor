feb8766b2006a4371c6c775e6528891d6be05461
Emily Worrall <worrall.emily@gmail.com>
2019-11-28 17:00:57 +0000

    Add failing feature spec for creating a bitmap
    This includes creating an input file containing a list of valid
    commands and expecting the output to be the final bitmap.

---
+++ spec/features/creating_a_bitmap_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+require "bitmap_editor"
+
+RSpec.describe "Creating a bitmap" do
+  subject(:bitmap_editor) { BitmapEditor.new }
+
+  before do
+    allow(STDOUT).to receive(:puts)
+  end
+
+  after do
+    File.delete(input_filename) if File.exists?(input_filename)
+  end
+
+  describe "Creating a bitmap with multiple edits" do
+    it "successfully creates a bitmap" do
+      create_file(
+        [
+          "I 5 6",
+          "L 1 3 A",
+          "V 2 3 6 W",
+          "H 3 5 2 Z",
+          "S"
+        ]
+      )
+      bitmap_editor.run(input_filename)
+
+      expect(STDOUT).to have_received(:puts).with(
+        [
+          "OOOOO",
+          "OOZZZ",
+          "AWOOO",
+          "OWOOO",
+          "OWOOO",
+          "OWOOO"
+        ]
+      )
+    end
+  end
+
+  def create_file(lines)
+    File.open(input_filename, "w+") do |file|
+      file.puts(lines)
+    end
+  end
+
+  def input_filename
+    "examples/test.txt"
+  end
+end
