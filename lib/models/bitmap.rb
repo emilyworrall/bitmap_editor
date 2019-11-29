b5cbce47ee29b313e5ec5f0f2ac898103dcaeaa4
Emily Worrall <worrall.emily@gmail.com>
2019-11-29 15:25:19 +0000

    Init Bitmap with all pixels set to white
    The Bitmap model has a grid instance variable that will be updated when
    more commands are implemented, but for now when Bitmap is initialised it
    creates a grid with all values equalling white.

--- lib/models/bitmap.rb
+++ lib/models/bitmap.rb
@@ -1,6 +1,30 @@
 class Bitmap
+  WHITE = "O".freeze
+
   def initialize(rows, cols)
     @rows = rows
     @cols = cols
+    @grid = {}
+
+    initialize_grid
+  end
+
+  attr_reader :rows, :cols, :grid
+  private :rows, :cols
+
+  private
+
+  def initialize_grid
+    coordinates = []
+
+    (1..cols).each do |x|
+      (1..rows).each do |y|
+        coordinates.push([x, y])
+      end
+    end
+
+    coordinates.each do |coordinate|
+      grid[coordinate] = WHITE
+    end
   end
 end
--- spec/lib/models/bitmap_spec.rb
+++ spec/lib/models/bitmap_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Bitmap do
   subject(:bitmap) { Bitmap.new(rows, cols) }
   let(:rows) { 3 }
   let(:cols) { 3 }
+
+  describe "#new" do
+    it "creates a grid of specified row and col size" do
+      expect(bitmap.grid.count).to eq(rows * cols)
+    end
+
+    it "fills all grid coordinates with colour white 'O'" do
+      expect(bitmap.grid.values.all?("O")).to eq(true)
+    end
+  end
 end
